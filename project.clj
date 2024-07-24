262ae639444301cc4cc1ee7add6a2a6731e0076e
Punit Naik <naik.punit44@gmail.com>
2024-07-25 00:51:28 +0530

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.punit-naik/quartzite-postgresql "1.1.0"
+(defproject org.clojars.punit-naik/quartzite-postgresql "1.1.1"
   :description "A small helper library to set up Quartzite scheduler with PostgreSQL as a store"
   :url "http://github.com/punit-naik/quartzite-postgresql"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
